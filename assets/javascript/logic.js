ec3d4917167d636d97315f08bce4427c2c51b060
Villejoint <kvillejo@estee.com>
2017-09-19 09:42:25 -0400

    Updates about and portfolio sections

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,12 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').addClass('shrink');
-    } else {
-      $('nav').removeClass('shrink');
-    }
-  });
+$(document).ready(function() {
+  $("[rel='tooltip']").tooltip();    
+
+  $('.thumbnail').hover(
+      function(){
+          $(this).find('.caption').slideDown(250); //.fadeIn(250)
+      },
+      function(){
+          $(this).find('.caption').slideUp(250); //.fadeOut(205)
+      }
+  ); 
+});
